e7855ee26abb17c28250eba1943099df5093b2dd
nyamatongwe <unknown>
2002-01-04 22:04:41 +0000

    Changed int constant to be smaller than maximum int.

--- gtk/SciTEGTK.cxx
+++ gtk/SciTEGTK.cxx
@@ -2443,7 +2443,7 @@ void SciTEGTK::CreateUI() {
 	                   GtkSignalFunc(MousePress), gthis);
 
 	gtk_window_set_title(GTK_WINDOW(PWidget(wSciTE)), appName);
-	const int useDefault = 0x100000000;
+	const int useDefault = 0x10000000;
 	int left = props.GetInt("position.left", useDefault);
 	int top = props.GetInt("position.top", useDefault);
 	int width = props.GetInt("position.width", useDefault);
