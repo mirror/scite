1bc5442434a2d2c2c109725610cefb69af83dd12
nyamatongwe <unknown>
2002-05-08 08:31:28 +0000

    Removed pdbs from zips.

--- zipsrc.bat
+++ zipsrc.bat
@@ -1,4 +1,4 @@
 cd ..
 del/q scite.zip
-zip scite.zip scintilla\*.* scintilla\*\*.* scite\*.* scite\*\*.* -x *.o -x *.obj -x *.dll -x *.exe
+zip scite.zip scintilla\*.* scintilla\*\*.* scite\*.* scite\*\*.* -x *.o -x *.obj -x *.dll -x *.exe -x *.pdb
 cd scite
