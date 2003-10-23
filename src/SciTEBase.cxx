df5540617adad9c943d58d9b79da3ce4df5632e3
nyamatongwe <unknown>
2003-10-23 21:52:07 +0000

    Updated for 1.56.

--- src/SciTEBase.cxx
+++ src/SciTEBase.cxx
@@ -408,13 +408,13 @@ void SciTEBase::SetAboutMessage(WindowID wsci, const char *appTitle) {
 		AddStyledText(wsci, "\n", 0);
 		SetAboutStyle(wsci, 1, ColourDesired(0, 0, 0));
 		AddStyledText(wsci, LocaliseString("Version").c_str(), 1);
-		AddStyledText(wsci, " 1.55\n", 1);
+		AddStyledText(wsci, " 1.56\n", 1);
 		SetAboutStyle(wsci, 2, ColourDesired(0, 0, 0));
 		Platform::SendScintilla(wsci, SCI_STYLESETITALIC, 2, 1);
 		AddStyledText(wsci, LocaliseString("by").c_str(), 2);
 		AddStyledText(wsci, " Neil Hodgson.\n", 2);
 		SetAboutStyle(wsci, 3, ColourDesired(0, 0, 0));
-		AddStyledText(wsci, "December 1998-September 2003.\n", 3);
+		AddStyledText(wsci, "December 1998-October 2003.\n", 3);
 		SetAboutStyle(wsci, 4, ColourDesired(0, 0x7f, 0x7f));
 		AddStyledText(wsci, "http://www.scintilla.org\n", 4);
 		SString translator = LocaliseString("TranslationCredit", false);
