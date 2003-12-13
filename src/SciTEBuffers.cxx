3cce0df674d00e3567d6a1d77cd1223f52f4faf4
nyamatongwe <unknown>
2003-12-13 23:45:05 +0000

    Removed styling done in UpdateBuffersCurrent.

--- src/SciTEBuffers.cxx
+++ src/SciTEBuffers.cxx
@@ -406,7 +406,7 @@ void SciTEBase::UpdateBuffersCurrent() {
 		buffers.buffers[currentbuf].unicodeMode = unicodeMode;
 
 		// retrieve fold state and store in buffer state info
-		SendEditor(SCI_COLOURISE, 0, -1);
+		//SendEditor(SCI_COLOURISE, 0, -1);
 		int maxLine = SendEditor(SCI_GETLINECOUNT);
 		int foldPoints = 0;
 
