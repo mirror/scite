192393bab272fc0f5633a8419d6c907e7884630b
nyamatongwe <unknown>
2004-01-31 22:43:36 +0000

    Made Find in Files dialog wider to fit reasonable directories and search string in Find dialog wider. Updated some variable names to fit conventions.

--- gtk/SciTEGTK.cxx
+++ gtk/SciTEGTK.cxx
@@ -1,6 +1,6 @@
 // SciTE - Scintilla based Text Editor
 // SciTEGTK.cxx - main code for the GTK+ version of the editor
-// Copyright 1998-2003 by Neil Hodgson <neilh@scintilla.org>
+// Copyright 1998-2004 by Neil Hodgson <neilh@scintilla.org>
 // The License.txt file describes the conditions under which this software may be distributed.
 
 #include <stdlib.h>
@@ -184,9 +184,9 @@ protected:
 	GtkWidget *comboDir;
 	GtkWidget *comboReplace;
 	GtkWidget *entryParam[maxParam];
-	GtkWidget *compile_btn;
-	GtkWidget *build_btn;
-	GtkWidget *stop_btn;
+	GtkWidget *btnCompile;
+	GtkWidget *btnBuild;
+	GtkWidget *btnStop;
 	GtkItemFactory *itemFactory;
 	GtkAccelGroup *accelGroup;
 
@@ -387,9 +387,9 @@ SciTEGTK::SciTEGTK(Extension *ext) : SciTEBase(ext) {
 	toggleReverse = 0;
 	comboFind = 0;
 	comboReplace = 0;
-	compile_btn = 0;
-	build_btn = 0;
-	stop_btn = 0;
+	btnCompile = 0;
+	btnBuild = 0;
+	btnStop = 0;
 	itemFactory = 0;
 
 	fileSelectorWidth = 580;
@@ -790,10 +790,10 @@ void SciTEGTK::CheckMenus() {
 	CheckAMenuItem(IDM_VIEWSTATUSBAR, sbVisible);
 	CheckAMenuItem(IDM_VIEWTABBAR, tabVisible);
 
-	if (build_btn) {
-		gtk_widget_set_sensitive(build_btn, !executing);
-		gtk_widget_set_sensitive(compile_btn, !executing);
-		gtk_widget_set_sensitive(stop_btn, executing);
+	if (btnBuild) {
+		gtk_widget_set_sensitive(btnBuild, !executing);
+		gtk_widget_set_sensitive(btnCompile, !executing);
+		gtk_widget_set_sensitive(btnStop, executing);
 	}
 }
 
@@ -1231,6 +1231,8 @@ void SciTEGTK::FindInFiles() {
 	                 row, row + 1, optse, opts, 5, 5);
 	gtk_widget_show(comboDir);
 	gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(comboDir)->entry), findInDir);
+	// Make a little wider than would happen automatically to show realistic paths
+	gtk_entry_set_width_chars(GTK_ENTRY(GTK_COMBO(comboDir)->entry), 40);
 	gtk_signal_connect(GTK_OBJECT(GTK_COMBO(comboDir)->entry),
 	                   "activate", GtkSignalFunc(FindInFilesSignal), this);
 	gtk_combo_disable_activate(GTK_COMBO(comboDir));
@@ -1620,6 +1622,7 @@ void SciTEGTK::FindReplace(bool replace) {
 	                 row, row + 1, optse, opts, 5, 5);
 	gtk_widget_show(comboFind);
 	gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(comboFind)->entry), findWhat.c_str());
+	gtk_entry_set_width_chars(GTK_ENTRY(GTK_COMBO(comboFind)->entry), 40);
 	gtk_entry_select_region(GTK_ENTRY(GTK_COMBO(comboFind)->entry), 0, findWhat.length());
 	gtk_signal_connect(GTK_OBJECT(GTK_COMBO(comboFind)->entry),
 	                   "activate", GtkSignalFunc(FRFindSignal), this);
@@ -2285,9 +2288,9 @@ void SciTEGTK::AddToolBar() {
 	AddToolButton("Replace", IDM_REPLACE, replace_xpm);
 
 	gtk_toolbar_append_space(GTK_TOOLBAR(PWidget(wToolBar)));
-	compile_btn = AddToolButton("Compile", IDM_COMPILE, compile_xpm);
-	build_btn = AddToolButton("Build", IDM_BUILD, build_xpm);
-	stop_btn = AddToolButton("Stop", IDM_STOPEXECUTE, stop_xpm);
+	btnCompile = AddToolButton("Compile", IDM_COMPILE, compile_xpm);
+	btnBuild = AddToolButton("Build", IDM_BUILD, build_xpm);
+	btnStop = AddToolButton("Stop", IDM_STOPEXECUTE, stop_xpm);
 
 	gtk_toolbar_append_space(GTK_TOOLBAR(PWidget(wToolBar)));
 	AddToolButton("Previous Buffer", IDM_PREVFILE, prev_xpm);
