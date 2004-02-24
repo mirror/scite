15c06cedb80fb9033eb812ba49d4a69f4412f89b
nyamatongwe <unknown>
2004-02-24 20:39:27 +0000

    Avoid gtk_entry_set_width_chars on GTK+ 1 as it was a new feature in GTK+ 2.

--- gtk/SciTEGTK.cxx
+++ gtk/SciTEGTK.cxx
@@ -1232,7 +1232,9 @@ void SciTEGTK::FindInFiles() {
 	gtk_widget_show(comboDir);
 	gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(comboDir)->entry), findInDir);
 	// Make a little wider than would happen automatically to show realistic paths
+#if GTK_MAJOR_VERSION >= 2
 	gtk_entry_set_width_chars(GTK_ENTRY(GTK_COMBO(comboDir)->entry), 40);
+#endif
 	gtk_signal_connect(GTK_OBJECT(GTK_COMBO(comboDir)->entry),
 	                   "activate", GtkSignalFunc(FindInFilesSignal), this);
 	gtk_combo_disable_activate(GTK_COMBO(comboDir));
@@ -1622,7 +1624,9 @@ void SciTEGTK::FindReplace(bool replace) {
 	                 row, row + 1, optse, opts, 5, 5);
 	gtk_widget_show(comboFind);
 	gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(comboFind)->entry), findWhat.c_str());
+#if GTK_MAJOR_VERSION >= 2
 	gtk_entry_set_width_chars(GTK_ENTRY(GTK_COMBO(comboFind)->entry), 40);
+#endif
 	gtk_entry_select_region(GTK_ENTRY(GTK_COMBO(comboFind)->entry), 0, findWhat.length());
 	gtk_signal_connect(GTK_OBJECT(GTK_COMBO(comboFind)->entry),
 	                   "activate", GtkSignalFunc(FRFindSignal), this);
