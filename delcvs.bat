16bd1838b952aa3447bdb8d3110583621b92d405
nyamatongwe <unknown>
2004-03-29 12:55:09 +0000

    Removing CVS directories under lua.

--- delcvs.bat
+++ delcvs.bat
@@ -2,6 +2,10 @@ rmdir /S /Q CVS
 rmdir /S /Q bin\CVS
 rmdir /S /Q boundscheck\CVS
 rmdir /S /Q doc\CVS
+rmdir /S /Q lua\CVS
+rmdir /S /Q lua\include\CVS
+rmdir /S /Q lua\src\CVS
+rmdir /S /Q lua\src\lib\CVS
 rmdir /S /Q gtk\CVS
 rmdir /S /Q scripts\CVS
 rmdir /S /Q src\CVS
