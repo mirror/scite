5f17b09fcd6c9f4b0af620e96a4585f3a81be2d0
nyamatongwe <unknown>
2005-06-07 23:20:59 +0000

    Added virtual destructot to avoid warning from GCC 4.

--- src/Extender.h
+++ src/Extender.h
@@ -13,6 +13,8 @@ class Accessor;
 
 class ExtensionAPI {
 public:
+	virtual ~ExtensionAPI() {
+	}
 	enum Pane { paneEditor=1, paneOutput=2, paneFindOutput=3 };
 	virtual sptr_t Send(Pane p, unsigned int msg, uptr_t wParam=0, sptr_t lParam=0)=0;
 	virtual char *Range(Pane p, int start, int end)=0;
