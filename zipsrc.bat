8a41570eea39330d3c5f32febeceff2a79c58dc2
nyamatongwe <unknown>
2005-07-04 02:04:15 +0000

    Remove more temporary build files from the set zipped.

--- zipsrc.bat
+++ zipsrc.bat
@@ -1,4 +1,4 @@
 cd ..
 del/q scite.zip
-zip scite.zip scintilla\*.* scintilla\*\*.* scite\*.* scite\*\*.* scite\*\*\*.* scite\*\*\*\*.* -x *.o -x *.obj -x *.lib -x *.dll -x *.exe -x *.pdb -x *.res -x *.exp
+zip scite.zip scintilla\*.* scintilla\*\*.* scite\*.* scite\*\*.* scite\*\*\*.* scite\*\*\*\*.* -x *.o -x *.obj -x *.lib -x *.dll -x *.exe -x *.pdb -x *.res -x *.exp -x *.ncb -x *.sbr -x *.ilk -x *.idb
 cd scite
