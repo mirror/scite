3a65371d9df5e214943d61dc9b3d2d8dc7186eb7
nyamatongwe <unknown>
2008-09-21 11:18:00 +0000

    Fix for bug #2118587: Does not reload buffer when exposed through closing another file.

--- src/SciTEBuffers.cxx
+++ src/SciTEBuffers.cxx
@@ -706,6 +706,7 @@ void SciTEBase::Close(bool updateUI, bool loadingSession, bool makingRoomForNew)
 		if (closingLast) {
 			ClearDocument();
 		}
+		CheckReload();
 		if (updateUI) {
 			RestoreState(bufferNext);
 			DisplayAround(bufferNext);
