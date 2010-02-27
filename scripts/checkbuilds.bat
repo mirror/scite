dd11f1ed0a5ff8e1c5f574d4edef53b65e00dbb5
nyamatongwe <unknown>
2010-02-27 04:17:16 +0000

    Home machine rebuild.

--- scripts/checkbuilds.bat
+++ scripts/checkbuilds.bat
@@ -8,8 +8,8 @@ rem
 cd ..\..
 set
 set BORLAND_BASE=C:\Borland\bcc55
-set MSDEV_BASE=C:\Program Files\Microsoft Visual Studio\Common\MSDev98\Bin
-set MSDEV71_BASE=C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools
+set MSDEV_BASE=D:\Program Files\Microsoft Visual Studio\Common\MSDev98\Bin
+set MSDEV71_BASE=D:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools
 rem
 rem ************************************************************
 rem Target 1: Borland C++ build
