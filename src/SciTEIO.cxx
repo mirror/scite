fd7e9f820d1f6c4071f3aba6a404a38f85eb46c6
nyamatongwe <unknown>
2011-06-06 12:28:01 +1000

    Ensure buffers aren't reused after release by setting to 0.

--- src/SciTEIO.cxx
+++ src/SciTEIO.cxx
@@ -733,8 +733,10 @@ int SciTEBase::SaveIfUnsureAll(bool forceQuestion) {
 	}
 	// Release all the extra documents
 	for (int j = 0; j < buffers.size; j++) {
-		if (buffers.buffers[j].doc)
+		if (buffers.buffers[j].doc) {
 			wEditor.Call(SCI_RELEASEDOCUMENT, 0, buffers.buffers[j].doc);
+			buffers.buffers[j].doc = 0;
+		}
 	}
 	// Initial document will be deleted when editor deleted
 	return IDYES;
