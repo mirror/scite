daef397ae314a4f7c88fbe7adaac3a8cd6dfec9b
nyamatongwe <unknown>
2011-10-27 09:49:10 +1100

    Updates for version 3.0.0.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "2.29"
-#define VERSION_WORDS 2, 2, 9, 0
-#define COPYRIGHT_DATES "December 1998-September 2011"
+#define VERSION_SCITE "3.0.0"
+#define VERSION_WORDS 3, 0, 0, 0
+#define COPYRIGHT_DATES "December 1998-November 2011"
 #define COPYRIGHT_YEARS "1998-2011"
 
 // Menu IDs.
