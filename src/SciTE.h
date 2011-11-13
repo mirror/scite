5fb5f073507c1d263b05322849d819aa232ea93f
nyamatongwe <unknown>
2011-11-13 19:24:33 +1100

    Preparing for 3.0.1 release.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.0.0"
-#define VERSION_WORDS 3, 0, 0, 0
+#define VERSION_SCITE "3.0.1"
+#define VERSION_WORDS 3, 0, 1, 0
 #define COPYRIGHT_DATES "December 1998-November 2011"
 #define COPYRIGHT_YEARS "1998-2011"
 
