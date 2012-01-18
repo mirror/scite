24e0a90184308aa214fe1e6ffd0336567d52809c
nyamatongwe <unknown>
2012-01-18 13:03:16 +1100

    Ensure menu dirty flags updated when files finish writing in background.

--- src/SciTEIO.cxx
+++ src/SciTEIO.cxx
@@ -444,6 +444,7 @@ void SciTEBase::TextWritten(FileWorker *pFileWorker) {
 				buffers.buffers[iBuffer].isDirty = false;
 				// Need to make writable and set save point when next receive focus.
 				buffers.AddFuture(iBuffer, Buffer::fdFinishSave);
+				SetBuffersMenu();
 			}
 		}
 	} else {
