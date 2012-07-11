b134c2bb2b9490e2b377b5d35f19908ca4e92a4f
nyamatongwe <unknown>
2012-07-11 11:41:20 +1000

    Updates for next release 3.2.1.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.2.0"
-#define VERSION_WORDS 3, 2, 0, 0
+#define VERSION_SCITE "3.2.1"
+#define VERSION_WORDS 3, 2, 1, 0
 #define COPYRIGHT_DATES "December 1998-June 2012"
 #define COPYRIGHT_YEARS "1998-2012"
 
