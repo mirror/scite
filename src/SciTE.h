b0c67cb5f857aafab7f328e208dcf902744c6b99
nyamatongwe <unknown>
2013-02-22 18:19:03 +1100

    Prepare for 3.2.5.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.2.4"
-#define VERSION_WORDS 3, 2, 4, 0
-#define COPYRIGHT_DATES "December 1998-January 2013"
+#define VERSION_SCITE "3.2.5"
+#define VERSION_WORDS 3, 2, 5, 0
+#define COPYRIGHT_DATES "December 1998-February 2013"
 #define COPYRIGHT_YEARS "1998-2013"
 
 // Menu IDs.
