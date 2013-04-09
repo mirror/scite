f2d3db202037939527c2874a3e9c0900c03e5fc4
nyamatongwe <unknown>
2013-04-09 13:53:41 +1000

    Updating for 3.3.1.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.3.0"
-#define VERSION_WORDS 3, 3, 0, 0
+#define VERSION_SCITE "3.3.1"
+#define VERSION_WORDS 3, 3, 1, 0
 #define COPYRIGHT_DATES "December 1998-March 2013"
 #define COPYRIGHT_YEARS "1998-2013"
 
