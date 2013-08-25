b713cdf18af713f120537269a6a71fc1819f15d7
Neil <nyamatongwe@gmail.com>
2013-08-26 09:32:44 +1000

    Updated version number and description for next release.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.3.4"
-#define VERSION_WORDS 3, 3, 4, 0
-#define COPYRIGHT_DATES "December 1998-July 2013"
+#define VERSION_SCITE "3.3.5"
+#define VERSION_WORDS 3, 3, 5, 0
+#define COPYRIGHT_DATES "December 1998-August 2013"
 #define COPYRIGHT_YEARS "1998-2013"
 
 // Menu IDs.
