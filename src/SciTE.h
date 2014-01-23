7273779c00fbcf20d29865fbf9a4227db386f7c3
Neil <nyamatongwe@gmail.com>
2014-01-24 09:00:07 +1100

    Updates for 3.3.8.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,10 +9,10 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.3.7"
-#define VERSION_WORDS 3, 3, 7, 0
-#define COPYRIGHT_DATES "December 1998-December 2013"
-#define COPYRIGHT_YEARS "1998-2013"
+#define VERSION_SCITE "3.3.8"
+#define VERSION_WORDS 3, 3, 8, 0
+#define COPYRIGHT_DATES "December 1998-January 2014"
+#define COPYRIGHT_YEARS "1998-2014"
 
 // Menu IDs.
 // These are located 100 apart. No one will want more than 100 in each menu ;)
