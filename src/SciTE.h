9ee0a58a3d8530135d2dab9fe65b55df04cc25ac
Neil <nyamatongwe@gmail.com>
2014-03-18 08:11:42 +1100

    Updating to get ready for 3.4.0.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.3.9"
-#define VERSION_WORDS 3, 3, 9, 0
-#define COPYRIGHT_DATES "December 1998-January 2014"
+#define VERSION_SCITE "3.4.0"
+#define VERSION_WORDS 3, 4, 0, 0
+#define COPYRIGHT_DATES "December 1998-March 2014"
 #define COPYRIGHT_YEARS "1998-2014"
 
 // Menu IDs.
