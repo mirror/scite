1062c2180f57d556e84aa020cb8525fcd78dd0cf
Neil <nyamatongwe@gmail.com>
2014-03-28 15:11:46 +1100

    Updating vesion number for next release.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.4.0"
-#define VERSION_WORDS 3, 4, 0, 0
-#define COPYRIGHT_DATES "December 1998-March 2014"
+#define VERSION_SCITE "3.4.1"
+#define VERSION_WORDS 3, 4, 1, 0
+#define COPYRIGHT_DATES "December 1998-April 2014"
 #define COPYRIGHT_YEARS "1998-2014"
 
 // Menu IDs.
