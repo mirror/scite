a821586bb8c42961a384a7e4040ca1cea59c3a1c
Neil <nyamatongwe@gmail.com>
2014-05-26 11:53:52 +1000

    Updates for 3.4.3 release.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "3.4.2"
-#define VERSION_WORDS 3, 4, 2, 0
+#define VERSION_SCITE "3.4.3"
+#define VERSION_WORDS 3, 4, 3, 0
 #define COPYRIGHT_DATES "December 1998-May 2014"
 #define COPYRIGHT_YEARS "1998-2014"
 
