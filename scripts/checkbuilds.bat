89504f0920fb71e42a246ad3a122e8c98b0289c3
Neil <nyamatongwe@gmail.com>
2016-10-21 10:45:36 +1100

    Remove uninteresting cppcheck warnings with some preprocessor definitions.

--- scripts/checkbuilds.bat
+++ scripts/checkbuilds.bat
@@ -105,7 +105,7 @@ rem ************************************************************
 rem Target 9: cppcheck
 call scite\scripts\clearboth
 cppcheck -j 8 --enable=all --suppressions-list=scintilla/cppcheck.suppress --max-configs=100 -I scintilla/src -I scintilla/include -I scintilla/lexlib -I scintilla/qt/ScintillaEditBase --template=gcc --quiet scintilla
-cppcheck -j 8 --enable=all --max-configs=100 -I scite/src -I scintilla/include -I scite/lua/include --template=gcc --quiet scite
+cppcheck -j 8 --enable=all --max-configs=100 -I scite/src -I scintilla/include -I scite/lua/include -Ulua_assert -DPATH_MAX=260 --template=gcc --quiet scite
 rem
 rem Finished
 call scite\scripts\clearboth
