33fa7338e67cd60e583ed2fb5833f833bdfe8515
Neil <nyamatongwe@gmail.com>
2017-10-18 13:21:57 +1100

    Updates for 4.0.1 release.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "4.0.0"
-#define VERSION_WORDS 4, 0, 0, 0
-#define COPYRIGHT_DATES "December 1998-August 2017"
+#define VERSION_SCITE "4.0.1"
+#define VERSION_WORDS 4, 0, 1, 0
+#define COPYRIGHT_DATES "December 1998-October 2017"
 #define COPYRIGHT_YEARS "1998-2017"
 
 // Menu IDs.
