1c49807b099fe81e5fa0ce8a1d49e00d1c00fceb
Neil Hodgson <nyamatongwe@gmail.com>
2018-01-28 09:30:55 +1100

    Switched build to C++17. Removed Clang sections from check build script as Linux Clang installations don't cleanly support C++17 yet.

--- scripts/checkbuildlinux.sh
+++ scripts/checkbuildlinux.sh
@@ -101,44 +101,44 @@ cd ../..
 
 # ************************************************************
 # Target 4: clang build for GTK+ 2
-cd scintilla/gtk
-make clean
-make $JOBS CLANG=1 CHECK_DEPRECATED=1
-cd ../..
+#~ cd scintilla/gtk
+#~ make clean
+#~ make $JOBS CLANG=1 CHECK_DEPRECATED=1
+#~ cd ../..
 
-cd scite/gtk
-make clean
-# Don't bother with CHECK_DEPRECATED on SciTE as the GTK+ 3.x code path fixes the deprecations
-make $JOBS CLANG=1
-cd ../..
+#~ cd scite/gtk
+#~ make clean
+#~ # Don't bother with CHECK_DEPRECATED on SciTE as the GTK+ 3.x code path fixes the deprecations
+#~ make $JOBS CLANG=1
+#~ cd ../..
 
 # ************************************************************
 # Target 5: clang build for GTK+ 3
-cd scintilla/gtk
-make clean
-make $JOBS CLANG=1 GTK3=1 CHECK_DEPRECATED=1
-cd ../..
+#~ cd scintilla/gtk
+#~ make clean
+#~ make $JOBS CLANG=1 GTK3=1 CHECK_DEPRECATED=1
+#~ cd ../..
 
-cd scite/gtk
-make clean
-make $JOBS CLANG=1 GTK3=1 CHECK_DEPRECATED=1
-cd ../..
+#~ cd scite/gtk
+#~ make clean
+#~ make $JOBS CLANG=1 GTK3=1 CHECK_DEPRECATED=1
+#~ cd ../..
 
 # ************************************************************
 # Target 6: clang analyze for GTK+ 2
-cd scintilla/gtk
-make clean
-make $JOBS CHECK_DEPRECATED=1 analyze
-cd ../..
+#~ cd scintilla/gtk
+#~ make clean
+#~ make $JOBS CHECK_DEPRECATED=1 analyze
+#~ cd ../..
 
-cd scite/gtk
-make clean
-make $JOBS analyze
-make clean
-cd ../..
-cd scintilla/gtk
-make clean
-cd ../..
+#~ cd scite/gtk
+#~ make clean
+#~ make $JOBS analyze
+#~ make clean
+#~ cd ../..
+#~ cd scintilla/gtk
+#~ make clean
+#~ cd ../..
 
 # ************************************************************
 # Target 7: cppcheck static checker
