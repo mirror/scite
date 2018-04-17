95477b64c43408074858b02adc5b421862d627e2
Neil Hodgson <nyamatongwe@gmail.com>
2018-04-18 09:37:44 +1000

    Enable cppcheck suppressions on Linux.

--- scripts/checkbuildlinux.sh
+++ scripts/checkbuildlinux.sh
@@ -144,4 +144,4 @@ cd ../..
 # Target 7: cppcheck static checker
 # Disabled as there are false warnings and some different style choices
 cppcheck --enable=all --max-configs=100 --suppressions-list=scintilla/cppcheck.suppress -I scintilla/src -I scintilla/include -I scintilla/lexlib -I scintilla/qt/ScintillaEditBase --template=gcc --quiet scintilla
-cppcheck --enable=all --max-configs=100 -I scite/src -I scintilla/include -I scite/lua/include -Ulua_assert -DPATH_MAX=260 --template=gcc --quiet scite
+cppcheck --enable=all --max-configs=100 --suppressions-list=scite/cppcheck.suppress -I scite/src -I scintilla/include -I scite/lua/include -Ulua_assert -DPATH_MAX=260 --template=gcc --quiet scite
