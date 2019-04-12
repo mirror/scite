c8c209912e82a66f725e11582a87d1c0b3a44536
Neil <nyamatongwe@gmail.com>
2019-04-13 07:30:58 +1000

    Preparing for 4.1.5 release.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,9 +9,9 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "4.1.4"
-#define VERSION_WORDS 4, 1, 4, 0
-#define COPYRIGHT_DATES "December 1998-March 2019"
+#define VERSION_SCITE "4.1.5"
+#define VERSION_WORDS 4, 1, 5, 0
+#define COPYRIGHT_DATES "December 1998-April 2019"
 #define COPYRIGHT_YEARS "1998-2019"
 
 // Menu IDs.
