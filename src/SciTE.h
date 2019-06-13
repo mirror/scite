6afbaf7f054e50d5b918e22514c62b4f03a46ddb
Neil <nyamatongwe@gmail.com>
2019-06-13 12:39:11 +1000

    Make a new release due to a regression with 4.1.6.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "4.1.6"
-#define VERSION_WORDS 4, 1, 6, 0
+#define VERSION_SCITE "4.1.7"
+#define VERSION_WORDS 4, 1, 7, 0
 #define COPYRIGHT_DATES "December 1998-June 2019"
 #define COPYRIGHT_YEARS "1998-2019"
 
