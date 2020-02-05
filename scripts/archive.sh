71346b0fdf99d8ba92523da04ec940f4bd3a7c04
Zufu Liu <unknown>
2020-02-05 22:20:46 +1100

    Feature [feature-requests:#1335] Archive just the repository files to scite.tgz.

---
+++ scripts/archive.sh
@@ -0,0 +1,16 @@
+# Up to parent directory of scite
+cd ../..
+
+# Archive both Scintilla and SciTE to scite-clean
+rm -rf scite-clean
+hg archive --repository scintilla scite-clean/scintilla
+hg archive --repository scite scite-clean/scite
+
+# tar both scintilla and scite into scite.tgz
+rm -f scite.tgz
+cd scite-clean
+tar -czf ../scite.tgz scintilla scite
+
+# Remove temporary directory
+cd ..
+rm -rf scite-clean
