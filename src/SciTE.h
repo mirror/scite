20fcf49041e701c79352b991eb78eaa611988fa7
Neil <nyamatongwe@gmail.com>
2020-06-02 11:37:35 +1000

    Updates for 4.4.2.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "4.4.0"
-#define VERSION_WORDS 4, 4, 0, 0
+#define VERSION_SCITE "4.4.2"
+#define VERSION_WORDS 4, 4, 2, 0
 #define COPYRIGHT_DATES "December 1998-June 2020"
 #define COPYRIGHT_YEARS "1998-2020"
 
