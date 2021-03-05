3534b98231b7f17a293e1fb3d7b5e20530f80e3f
Neil <nyamatongwe@gmail.com>
2021-03-05 17:34:09 +1100

    Update version number to 5.0.0.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,8 +9,8 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "4.4.6"
-#define VERSION_WORDS 4, 4, 6, 0
+#define VERSION_SCITE "5.0.0"
+#define VERSION_WORDS 5, 0, 0, 0
 #define COPYRIGHT_DATES "December 1998-March 2021"
 #define COPYRIGHT_YEARS "1998-2021"
 
