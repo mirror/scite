87ea5a0ae21cc50e792894032b912d769b812dee
Neil <nyamatongwe@gmail.com>
2021-06-18 08:52:32 +1000

    Updated for stable version 5.1.0.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,12 +9,12 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "5.0.3"
-#define VERSION_WORDS 5, 0, 3, 0
+#define VERSION_SCITE "5.1.0"
+#define VERSION_WORDS 5, 1, 0, 0
 #define COPYRIGHT_DATES "December 1998-June 2021"
 #define COPYRIGHT_YEARS "1998-2021"
-#define VERSION_SCINTILLA "5.0.3"
-#define VERSION_LEXILLA "5.0.3"
+#define VERSION_SCINTILLA "5.1.0"
+#define VERSION_LEXILLA "5.1.0"
 
 // Menu IDs.
 // These are located 100 apart. No one will want more than 100 in each menu ;)
