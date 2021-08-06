c96125140adbb407b722793685e1e547aa547ab9
Neil Hodgson <nyamatongwe@gmail.com>
2021-08-07 09:45:06 +1000

    Rename filter to fileFilter to avoid shadowing from SciTEBase.

--- gtk/SciTEGTK.cxx
+++ gtk/SciTEGTK.cxx
@@ -1586,12 +1586,12 @@ bool SciTEGTK::SaveAsXXX(FileFormat fmt, const char *title, const char *ext) {
 		gtk_dialog_set_default_response(GTK_DIALOG(dlg), GTK_RESPONSE_ACCEPT);
 		FilePath savePath = SaveName(ext);
 		if (ext) {
-			GtkFileFilter *filter = gtk_file_filter_new();
-			gtk_file_filter_set_name(filter, ext+1);
+			GtkFileFilter *fileFilter = gtk_file_filter_new();
+			gtk_file_filter_set_name(fileFilter, ext+1);
 			std::string filterSTR = "*";
 			filterSTR += ext;
-			gtk_file_filter_add_pattern(filter, filterSTR.c_str());
-			gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dlg), filter);
+			gtk_file_filter_add_pattern(fileFilter, filterSTR.c_str());
+			gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dlg), fileFilter);
 			gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dlg), savePath.Directory().AsInternal());
 			gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dlg), savePath.Name().AsInternal());
 		} else if (savePath.IsUntitled()) { // saving 'untitled'
@@ -1652,10 +1652,10 @@ void SciTEGTK::LoadSessionDialog() {
 				      SCITE_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
 				      NULL);
 
-		GtkFileFilter *filter = gtk_file_filter_new();
-		gtk_file_filter_set_name(filter, "session");
-		gtk_file_filter_add_pattern(filter, "*.session");
-		gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dlg), filter);
+		GtkFileFilter *fileFilter = gtk_file_filter_new();
+		gtk_file_filter_set_name(fileFilter, "session");
+		gtk_file_filter_add_pattern(fileFilter, "*.session");
+		gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dlg), fileFilter);
 		gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dlg), filePath.Directory().AsInternal());
 		gtk_window_set_default_size(GTK_WINDOW(dlg), fileSelectorWidth, fileSelectorHeight);
 		if (gtk_dialog_run(GTK_DIALOG(dlg)) == GTK_RESPONSE_ACCEPT) {
@@ -1679,10 +1679,10 @@ void SciTEGTK::SaveSessionDialog() {
 				      SCITE_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
 				      NULL);
 
-		GtkFileFilter *filter = gtk_file_filter_new();
-		gtk_file_filter_set_name(filter, "session");
-		gtk_file_filter_add_pattern(filter, "*.session");
-		gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dlg), filter);
+		GtkFileFilter *fileFilter = gtk_file_filter_new();
+		gtk_file_filter_set_name(fileFilter, "session");
+		gtk_file_filter_add_pattern(fileFilter, "*.session");
+		gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dlg), fileFilter);
 		gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dlg), filePath.Directory().AsInternal());
 		gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dlg), "SciTE.session");
 		gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dlg), TRUE);
