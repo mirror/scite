d89b1d1a7e148f2a63f50214d79af6d2b85142e5
Neil <nyamatongwe@gmail.com>
2023-02-09 15:40:15 +1100

    Make zip filter out more build artifacts and not require later deletion of .hg directories.

--- zipsrc.bat
+++ zipsrc.bat
@@ -1,7 +1,6 @@
 cd ..
 del/q scite.zip
-zip scite.zip lexilla\*.* lexilla\*\*.* lexilla\*\*\*.* lexilla\*\*\*\*.* lexilla\*\*\*\*\*.*  scintilla\*.* scintilla\*\*.* scintilla\*\*\*.* scintilla\*\*\*\*.* scintilla\*\*\*\*\*.* scite\*.* scite\*\*.* scite\*\*\*.* scite\*\*\*\*.* -x *.o -x *.obj -x *.a -x *.lib -x *.dll -x *.exe -x *.pdb -x *.i -x *.res -x *.exp -x *.ncb -x *.sbr -x *.bsc -x *.tlog -x *.ilk -x *.ipch -x *.idb -x *.sdf -x *.dpsession -x *.bak
-zip scite.zip -d scintilla/.hg/*
-zip scite.zip -d lexilla/.hg/*
-zip scite.zip -d scite/.hg/*
+zip scite.zip lexilla\*.* lexilla\*\*.* lexilla\*\*\*.* lexilla\*\*\*\*.* lexilla\*\*\*\*\*.* scintilla\*.* scintilla\*\*.* scintilla\*\*\*.* scintilla\*\*\*\*.* scintilla\*\*\*\*\*.* scite\*.* scite\*\*.* scite\*\*\*.* scite\*\*\*\*.* ^
+ -x *.o *.obj *.a *.lib *.dll *.exe *.pdb *.i *.res *.exp *.ncb *.sbr *.bsc *.tlog *.ilk *.ipch *.idb *.sdf *.dpsession *.bak *.tgz ^
+ **/__pycache__/* **/Debug/* **/Release/* **/x64/* **/ARM64/* **/cov-int/* */.hg/* @
 cd scite
