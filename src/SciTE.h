6d1aa9061df892c3e16c2209b04e4b4419e0f051
Neil <nyamatongwe@gmail.com>
2024-03-02 19:33:12 +1100

    Updates for version 5.4.2.

--- src/SciTE.h
+++ src/SciTE.h
@@ -9,12 +9,12 @@
 #define SCITE_H
 
 // Version numbers and dates
-#define VERSION_SCITE "5.4.1"
-#define VERSION_WORDS 5, 4, 1, 0
-#define COPYRIGHT_DATES "December 1998-December 2023"
-#define COPYRIGHT_YEARS "1998-2023"
-#define VERSION_SCINTILLA "5.4.1"
-#define VERSION_LEXILLA "5.3.0"
+#define VERSION_SCITE "5.4.2"
+#define VERSION_WORDS 5, 4, 2, 0
+#define COPYRIGHT_DATES "December 1998-March 2024"
+#define COPYRIGHT_YEARS "1998-2024"
+#define VERSION_SCINTILLA "5.4.2"
+#define VERSION_LEXILLA "5.3.1"
 
 // Menu IDs.
 // These are located 100 apart. No one will want more than 100 in each menu ;)
